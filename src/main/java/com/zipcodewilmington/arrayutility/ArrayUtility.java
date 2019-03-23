0fcef99d08bc21e48a86951f30ed2074338b1cd9
Alexandra Bozzini <alexandrabozzini@zipcodersmbp74.lan>
2019-03-23 17:36:35 -0400

    Finished get most common from merge tests

--- src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
+++ src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
@@ -1,5 +1,10 @@
 package com.zipcodewilmington.arrayutility;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Created by leon on 3/6/18.
  */
@@ -27,7 +32,27 @@ public class ArrayUtility<SomeType> {
     }
 
     public SomeType getMostCommonFromMerge(SomeType[] arrayToMerge){
-        return null;
+        ArrayUtility<SomeType> util = new ArrayUtility<>(inputArray);
+        List<SomeType> list = new ArrayList<>();
+        list.addAll(Arrays.asList(inputArray));
+        list.addAll(Arrays.asList(arrayToMerge));
+        Integer count = 1, tempCount;
+        SomeType mode = list.get(0);
+        SomeType temp;
+        for (int i = 0; i < list.size() - 1; i++){
+            temp = list.get(i);
+            tempCount = 0;
+            for (int j = 1; j < list.size(); j++){
+                if (temp == list.get(j)){
+                    tempCount++;
+                }
+            }
+            if (tempCount > count){
+                mode = temp;
+                count = tempCount;
+            }
+        }
+        return mode;
     }
 
     public Integer getNumberOfOccurrences(SomeType valueToEvaluate){
