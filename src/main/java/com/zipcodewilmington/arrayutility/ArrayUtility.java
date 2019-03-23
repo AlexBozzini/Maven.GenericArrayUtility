b6dc5b58a924000e4b6a89e08e027334143cd942
Alexandra Bozzini <alexandrabozzini@zipcodersmbp74.lan>
2019-03-23 17:10:02 -0400

    Stubbed out methods, got project to compile

--- src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
+++ src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
@@ -3,5 +3,19 @@ package com.zipcodewilmington.arrayutility;
 /**
  * Created by leon on 3/6/18.
  */
-public class ArrayUtility {
+public class ArrayUtility<SomeType> {
+    public ArrayUtility(SomeType[] inputArray){}
+
+    public Integer countDuplicatesInMerge(SomeType[] arrayToMerge, SomeType valueToEvaluate){
+
+        return null;
+    }
+
+    public SomeType getMostCommonFromMerge(SomeType[] arrayToMerge){
+        return null;
+    }
+
+    public Integer getNumberOfOccurrences(SomeType valueToEvaluate){
+        return null;
+    }
 }
